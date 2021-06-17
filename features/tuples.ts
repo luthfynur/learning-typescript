8d4192a81b4852fccafd2dc10f49ce86290b5b20
luthfynur <nurluthfy98@gmail.com>
2021-06-18 00:42:02 +0700

    Tuples

---
+++ features/tuples.ts
@@ -0,0 +1,11 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 69];
+const sprite: Drink = ['clear', true, 69];
